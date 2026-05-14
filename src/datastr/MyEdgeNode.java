9d0ba4f9e6ca1843b4f166fccb21827ef23aecf8
ginterssmil <smiltinsginters@gmail.com>
2026-05-14 11:35:34 +0300

    Add: datastr MyEdgeNode

--- src/datastr/MyEdgeNode.java
+++ src/datastr/MyEdgeNode.java
@@ -1,5 +1,51 @@
 package datastr;
 
 public class MyEdgeNode {
-
+	private int indexToVertice;
+	private float weight; //TODO var taisit ar citu tipu
+	private MyEdgeNode nextEdge = null;
+	
+	
+	public int getIndexToVertice() {
+		return indexToVertice;
+	}
+	public float getWeight() {
+		return weight;
+	}
+	public MyEdgeNode getNextEdge() {
+		return nextEdge;
+	}
+	
+	
+	public void setIndexToVertice(int indexToVertice) {
+		if(indexToVertice>=0) {
+			this.indexToVertice = indexToVertice;
+		}
+		else {
+			this.indexToVertice = 0;
+		}
+	}
+	public void setWeight(float weight) {
+		if(weight > 0) {
+			this.weight = weight;
+		}
+		else {
+			this.weight = 1;
+		}
+	}
+	public void setNextEdge(MyEdgeNode nextEdge) {
+		this.nextEdge = nextEdge;
+	}
+	
+	public MyEdgeNode(int indexToVertice, float weight) {
+		setIndexToVertice(indexToVertice);
+		setWeight(weight);
+		
+	}
+	
+	public String toString() {
+		return "indexTo:" + indexToVertice + "(" + weight + ")";
+	}
+	
+	
 }
